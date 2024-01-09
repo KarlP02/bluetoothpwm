19bb488ba6ccefbc0a54864a103396a81e3d2963
Karl Piiber <90192557+KarlP02@users.noreply.github.com>
2024-01-09 16:21:25 +0200

    add account page
    added add account page, no functionality

--- bluetooth_password_manager/src/Pages/WelcomePage.java
+++ bluetooth_password_manager/src/Pages/WelcomePage.java
@@ -7,12 +7,17 @@ import java.awt.event.ActionListener;
 public class WelcomePage implements ActionListener {
     JFrame frame = new JFrame();
     JLabel welcomeLabel = new JLabel("Hello, ");
+    JButton addButton = new JButton("add");
 
     WelcomePage(String userID) {
         welcomeLabel.setBounds(10, 10, 200, 35);
         welcomeLabel.setText("Hello, " + userID + "!");
 
+        addButton.setBounds(10, 50, 100, 25);
+        addButton.addActionListener(this);
+
         frame.add(welcomeLabel);
+        frame.add(addButton);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1280, 720);
@@ -23,6 +28,8 @@ public class WelcomePage implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (e.getSource() == addButton) {
+            AddPage addPage = new AddPage();
+        }
     }
 }
